85606ab9fed9e51cd856df325c9a7f4771c7dc89
Miikka Koskinen <miikka.koskinen@vapaus.org>
2009-09-20 19:51:35 +0300

    Add gnuplot formula.

---
+++ Library/Formula/gnuplot.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Gnuplot <Formula
+  @url='http://downloads.sourceforge.net/project/gnuplot/gnuplot/4.2.6/gnuplot-4.2.6.tar.gz'
+  @homepage='http://www.gnuplot.info/'
+  @md5='c10468d74030e8bed0fd6865a45cf1fd'
+  
+  def deps
+  	LibraryDep.new 'gnuplot'
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-readline=#{prefix}/lib"
+    system "make install"
+  end
+end
